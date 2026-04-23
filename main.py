dd38ad5165f2e3bad22d36be02d6854604baa4d8
akshay-joseph-jimmy <akshayjosephjimmy05@gmail.com>
2026-04-23 10:42:47 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+
+import os
+from fastapi import FastAPI,HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from supabase import create_client,Client
+from routes import users
+
+load_dotenv()
+
+
+
+app = FastAPI(title="RAG API", description="API for Retrieval-Augmented Generation", version="1.0"  )
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the RAG API!"}
+
+app.include_router(users.router)
+
+
+
+    
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app,host="0.0.0.0",port=8000,reload=True)
+    
